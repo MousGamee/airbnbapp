8e16118759e0d76a9d6fc4509bf03f98aa5e0c40
Fatou Fofana <fatoufofana@MacBook-Air-de-Fatou.local>
2024-02-20 23:14:14 +0100

    set up tab bar

--- app/(tabs)/Profil.tsx
+++ app/(tabs)/Profil.tsx
@@ -1,12 +1,12 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 
-const index = () => {
+const Profil = () => {
   return (
     <View>
-      <Text>index</Text>
+      <Text>Profil</Text>
     </View>
   )
 }
 
-export default index
+export default Profil
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -22,7 +22,9 @@ SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const [loaded, error] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    'mon': require('../assets/fonts/Montserrat-Regular.ttf'),
+    'mon-sb': require('../assets/fonts/SpaceMono-Regular.ttf'),
+    'mon-b': require('../assets/fonts/SpaceMono-Regular.ttf'),
     ...FontAwesome.font,
   });
 
