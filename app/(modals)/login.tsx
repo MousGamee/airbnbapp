8a7f2ba3218edb86540b94425a4641af8399a7c5
Fatou Fofana <fatoufofana@MacBook-Air-de-Fatou.local>
2024-02-22 01:07:25 +0100

    end of tabBar

--- app/(modals)/login.tsx
+++ app/(modals)/login.tsx
@@ -1,12 +1,102 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet, TextInput, TouchableOpacity } from 'react-native'
 import React from 'react'
+import { useWarmUpBrowser } from '../hooks/useWarmUpBrowser'
+import Colors from '@/constants/Colors'
+import { defaultStyles } from '@/constants/Styles'
+import { FontAwesome, Ionicons } from '@expo/vector-icons'
 
 const Page = () => {
+  useWarmUpBrowser()
   return (
-    <View>
-      <Text>login</Text>
+    <View style={styles.container}>
+     <TextInput
+        autoCapitalize="none"
+        placeholder="Email"
+        placeholderTextColor={Colors.grey}
+        style={[defaultStyles.inputField, { marginBottom: 30, color : '#000' }]}
+      />
+
+      <TouchableOpacity style={defaultStyles.btn}>
+        <Text style={defaultStyles.btnText}>Continue</Text>
+      </TouchableOpacity>
+
+      <View style={styles.seperatorView}>
+        <View
+          style={{
+            flex: 1,
+            borderBottomColor: 'black',
+            borderBottomWidth: StyleSheet.hairlineWidth,
+          }}
+        />
+        <Text style={styles.seperator}>or</Text>
+        <View
+          style={{
+            flex: 1,
+            borderBottomColor: 'black',
+            borderBottomWidth: StyleSheet.hairlineWidth,
+          }}
+        />
+      </View>
+
+      <View style={{ gap: 20 }}>
+        <TouchableOpacity style={styles.btnOutline}>
+          <FontAwesome name="phone" size={24} style={defaultStyles.btnIcon} />
+          <Text style={styles.btnOutlineText}>Continue with Phone</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.btnOutline} onPress={() => console.log('apple')}>
+          <Ionicons name="logo-apple" size={24} style={defaultStyles.btnIcon} />
+          <Text style={styles.btnOutlineText}>Continue with Apple</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.btnOutline} onPress={() => console.log('google')}>
+          <Ionicons name="logo-google" size={24} style={defaultStyles.btnIcon} />
+          <Text style={styles.btnOutlineText}>Continue with Google</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.btnOutline} onPress={() => console.log('face book')}>
+          <Ionicons name="logo-facebook" size={24} style={defaultStyles.btnIcon} />
+          <Text style={styles.btnOutlineText}>Continue with Facebook</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   )
 }
 
 export default Page
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    padding: 26,
+  },
+
+  seperatorView: {
+    flexDirection: 'row',
+    gap: 10,
+    alignItems: 'center',
+    marginVertical: 30,
+  },
+  seperator: {
+    fontFamily: 'mon-sb',
+    color: Colors.grey,
+    fontSize: 16,
+  },
+  btnOutline: {
+    backgroundColor: '#fff',
+    borderWidth: 1,
+    borderColor: Colors.grey,
+    height: 50,
+    borderRadius: 8,
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'row',
+    paddingHorizontal: 10,
+  },
+  btnOutlineText: {
+    color: '#000',
+    fontSize: 16,
+    fontFamily: 'mon-sb',
+  },
+});
--- app/(tabs)/Profil.tsx
+++ app/(tabs)/Profil.tsx
@@ -1,12 +1,21 @@
-import { View, Text } from 'react-native'
+import { View, Text, Button } from 'react-native'
 import React from 'react'
+import { useAuth } from '@clerk/clerk-expo'
+import { Link } from 'expo-router'
 
 const Profil = () => {
-  return (
-    <View>
-      <Text>Profil</Text>
-    </View>
-  )
+    const {signOut, isSignedIn} = useAuth()
+    return (
+        <View style={{
+            width: '100%',
+            height: '100%',
+            justifyContent: 'center',
+            alignItems: 'center'
+        }}>
+            <Button title='log out' onPress={() => signOut()} />
+            {!isSignedIn && <Link href={'../(modals)/login'} > login </Link>}
+        </View>
+    )
 }
 
 export default Profil
